2758fea7bd3a4c046878956c39cabb80704f34d7
Anthony Ardi Sukotjo <sukotjo33@yahoo.com.sg>
2020-12-22 17:30:00 +0800

    Set up basic settings page

--- lib/settings.dart
+++ lib/settings.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:LessApp/styles.dart';
 import 'package:http/http.dart' as http;
 import 'package:LessApp/wasteless-data.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingsPage extends StatefulWidget{
   @override
@@ -16,7 +17,35 @@ class SettingsPageState extends State<SettingsPage>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Styles.MainStatsPageHeader("Settings", FontWeight.bold, Colors.black),
-      body: Text("test"),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: 'Account',
+            tiles: [
+              SettingsTile(
+                title: 'Sign Out',
+                leading: Icon(Icons.language),
+                onPressed: (BuildContext context) {},
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: 'Misc',
+            tiles: [
+              SettingsTile(
+                title: 'Terms of Services',
+                leading: Icon(Icons.language),
+                onPressed: (BuildContext context) {},
+              ),
+              SettingsTile(
+                title: 'About Us',
+                leading: Icon(Icons.language),
+                onPressed: (BuildContext context) {},
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
