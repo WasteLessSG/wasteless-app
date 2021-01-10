5cbc44e78d9f3686047ff2cfa818d461a70130a3
Anthony Ardi Sukotjo <sukotjo33@yahoo.com.sg>
2021-01-11 03:23:55 +0800

    changed main method for debugging

--- lib/main.dart
+++ lib/main.dart
@@ -4,37 +4,37 @@ import 'package:flutter/material.dart';
 import 'package:WasteLess/login/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-// void main() {
-//
-//   runApp(
-//     new MaterialApp(
-// theme: ThemeData(
-// primaryColor: Colors.green[900],
-// ),
-//     home: Login(),
-//     debugShowCheckedModeBanner: false,
-//   ));
-//
-// }
+void main() {
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-// Test User Account
-  try {
-    FirebaseUser user = (await FirebaseAuth.instance.signInWithEmailAndPassword(email: "user@c.com", password: "password")).user;
-    runApp(
+  runApp(
     new MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.green[900],
-      ),
-        home: HomePage(user),
-    // home: Login(),
+theme: ThemeData(
+primaryColor: Colors.green[900],
+),
+    home: Login(),
     debugShowCheckedModeBanner: false,
   ));
-  } catch (e) {
-
-    print("ERROR" + e.message );
-  }
 
 }
+
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+// // Test User Account
+//   try {
+//     FirebaseUser user = (await FirebaseAuth.instance.signInWithEmailAndPassword(email: "user@c.com", password: "password")).user;
+//     runApp(
+//     new MaterialApp(
+//       theme: ThemeData(
+//         primaryColor: Colors.green[900],
+//       ),
+//         home: HomePage(user),
+//     // home: Login(),
+//     debugShowCheckedModeBanner: false,
+//   ));
+//   } catch (e) {
+//
+//     print("ERROR" + e.message );
+//   }
+//
+// }
